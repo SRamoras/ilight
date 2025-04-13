7fe72b774d6d490b1a102525f6f770cb408ff44c
S1lva <diogomcsilva5@gmail.com>
2025-04-13 08:21:48 +0100

    Backup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/mainlayout';
 import MainPage from './pages/mainpage';
 import GalleryPage from './pages/gallerypage';
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/ilight/', // altere para o nome do seu repositório
+  plugins: [react()]
 })
